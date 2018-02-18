06ac8173d25dea226b15ea864e4be148db5c161a
dobson <dobsonbox@hotmail.com>
2018-02-17 17:17:31 -0800

    added readme and started pg-connector

---
+++ server/README.md
@@ -0,0 +1,55 @@
+# dem01insurance
+# Author: _
+
+1. PURPOSE
+2. INSTRUCTIONS
+3. COMMENTS
+
+1. PURPOSE
+================================================================================
+The purpose of this project is to demonstrate integrating multiple technologies:
+    BACKEND
+    - GraphQL server, schema, and resolvers
+    - PostgreSQL database integration with NodeJS
+    * (future) - Python integration with NodeJS and PostgreSQL
+    FRONTEND
+    - Webpack
+    - ReactJS
+    - Redux
+    - ApolloClient
+    * (future) - data visualization
+    * (future) - other complicated data stuff
+
+
+2. INSTRUCTIONS
+================================================================================
+STEP 1 (setup):
+You should have NodeJS installed on your computer.
+You should have a PostgreSQL server running on port 5432.
+The GraphQL endpoint is 'http://localhost:4000/graphql'.
+The GraphQL schema has a 'createDB' mutation which:
+    (i)  Creates a database called 'demo_insurancedb'.
+    (ii) Creates and populates tables from csv files in the 'data' folder.
+
+STEP 2: 
+Run the GraphQL server:
+    (i)  Open command line in 'server' folder and type 'npm start'.
+    (ii) Wait for message 'GraphQL server running on port 4000...'.
+Create the database:
+    (i) ...
+
+3. COMMENTS
+================================================================================
+This is a demo and shouldn't actually be used: it lacks proper security (eg web tokens) and is ugly and lacks proper testing and real world testing and refining.
+This is not intended as the best solution for a company database and login system, just to demonstrate a way that multiple technologies can be used together.
+
+
+
+
+
+
+
+
+
+
+
---
+++ server/src/pg-connector.js
@@ -0,0 +1,116 @@
+import Sequelize from 'sequelize'
+
+const Op = Sequelize.Op;
+const operatorsAliases = {
+  eq: Op.eq,
+  ne: Op.ne,
+  gte: Op.gte,
+  gt: Op.gt,
+  lte: Op.lte,
+  lt: Op.lt,
+  not: Op.not,
+  in: Op.in,
+  notIn: Op.notIn,
+  is: Op.is,
+  like: Op.like,
+  notLike: Op.notLike,
+  iLike: Op.iLike,
+  notILike: Op.notILike,
+  regexp: Op.regexp,
+  notRegexp: Op.notRegexp,
+  iRegexp: Op.iRegexp,
+  notIRegexp: Op.notIRegexp,
+  between: Op.between,
+  notBetween: Op.notBetween,
+  overlap: Op.overlap,
+  contains: Op.contains,
+  contained: Op.contained,
+  adjacent: Op.adjacent,
+  strictLeft: Op.strictLeft,
+  strictRight: Op.strictRight,
+  noExtendRight: Op.noExtendRight,
+  noExtendLeft: Op.noExtendLeft,
+  and: Op.and,
+  or: Op.or,
+  any: Op.any,
+  all: Op.all,
+  values: Op.values,
+  col: Op.col
+}
+
+const db = new Sequelize('postgres://dbuser01@localhost:5432/demo_insurancedb', { operatorsAliases })
+
+db.authenticate().then(console.log('db authenticated...'))
+                 .catch(err => console.log(err))
+
+const Policy = db.define('policy',
+    {
+        policyNumber:   { type: Sequelize.INTEGER, primaryKey: true },
+        name:           { type: Sequelize.STRING },
+        dob:            { type: Sequelize.STRING },
+        gender:         { type: Sequelize.STRING },
+        smokingStatus:  { type: Sequelize.STRING },
+        faceAmount:     { type: Sequelize.INTEGER }
+    },
+    {
+        tableName: 'policies',
+        timestamps: false
+    }
+)
+
+const Agent = db.define('agent',
+    {
+        agentID:        { type: Sequelize.INTEGER, primaryKey: true },
+        password:       { type: Sequelize.STRING },
+        name:           { type: Sequelize.STRING },
+        policies:       { type: Sequelize.ARRAY(Sequelize.INTEGER) }
+    },
+    {
+        tableName: 'agents',
+        timestamps: false
+    }
+)
+
+const Employee = db.define('employee',
+    {
+        eeID:       { type: Sequelize.INTEGER, primaryKey: true },
+        password:   { type: Sequelize.STRING },
+        name:       { type: Sequelize.STRING }
+    },
+    {
+        tableName: 'employees',
+        timestamps: false
+    }
+)
+
+const getPolicy = pn => Policy.findById(pn).then(p => p)
+const getAgent = id => Agent.findById(id).then(a => a)
+const getEmployee = id => Employee.findById(id).then(ee => ee)
+
+export {    getPolicy, 
+            getAgent, 
+            getEmployee 
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
